6896af004ec7222e457cc1cc812092c6b2d93af0
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2022-09-21 17:34:37 -0300

    nueva clase Mensaje

--- Chat/src/red/Servidor.java
+++ Chat/src/red/Servidor.java
@@ -27,10 +27,12 @@ public class Servidor{
     public void iniciar(int puerto){
         System.out.println("Iniciando el servidor en el puerto "+ puerto);
         try {
-            
+            //Se inicializa el socket del servidor
             socketServidor = new ServerSocket(puerto);
             System.out.println(tiempo.marcaTiempo()+"Servidor iniciado");
             
+            //Se habilita la conexión de un cliente y se espera a que se realice
+            // la conexión
             socketCliente = socketServidor.accept();
             System.out.println(tiempo.marcaTiempo()+"Se ha conectado un usuario desde la IP: "+
                     socketCliente.getInetAddress().getHostAddress());
@@ -42,8 +44,10 @@ public class Servidor{
             //Salida de datos hacia el cliente conectado          
             salida = new PrintWriter(socketCliente.getOutputStream(), true);
             
+            //Preparamos un String para los datos de entrada desde el cliente
             String mensaje = "";
             
+            //Abrimos un bucle que se cierra con un mensaje específico
             while (!mensaje.equalsIgnoreCase("salir")) {                
                 mensaje = entrada.readLine();
                 System.out.println( 
@@ -51,10 +55,6 @@ public class Servidor{
                         socketCliente.getInetAddress().getHostAddress() + 
                         ":" + mensaje );
             }
-                    
-                    
-                    ;
-            
         } 
         catch (IOException e) {
             System.out.println("ERROR al iniciar el servidor: "+e.toString());
@@ -74,7 +74,9 @@ public class Servidor{
         
     }
     
-     
+     /**
+      * Permite iniciar el servid    * @param args 
+      */
      public static void main(String[] args) {
         Servidor servidor = new Servidor();
         servidor.iniciar(6666);
